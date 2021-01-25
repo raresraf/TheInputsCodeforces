59c08210d0c5826be637e85c7746128cde0a71d0
raresraf <raresfolea97@gmail.com>
2021-01-25 21:26:46 +0200

    670A

---
+++ 670A/generator/generator.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+if __name__ == '__main__':
+	test_cases = np.linspace(1, 1000000, num=50)
+	for test_case in test_cases:
+		with open(f"../{int(test_case)}.DAT", 'w') as f:
+			f.write(f"{int(test_case)}")
+
