efc9378ed5805a62876a6029842ccbf451d52603
raresraf <raresfolea97@gmail.com>
2021-06-01 14:39:27 +0300

    96A

---
+++ 96A/generator/generator.py
@@ -0,0 +1,15 @@
+import numpy as np
+import random
+import string
+
+if __name__ == '__main__':
+	test_cases = np.linspace(1, 100, num=50)
+	for test_case in test_cases:
+		with open(f"../{int(test_case)}.DAT", 'w') as f:
+			for x in range(int(test_case)):
+				l = np.random.randint(0, 4)
+				if int(l) == 0:
+					f.write(f"0")
+				else:
+					f.write(f"1")
+
