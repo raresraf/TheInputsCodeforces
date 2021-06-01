dff031056469425f60ecf030bf04776be0b8a826
raresraf <raresfolea97@gmail.com>
2021-06-01 12:28:02 +0300

    112A

---
+++ 112A/generator/generator.py
@@ -0,0 +1,11 @@
+import numpy as np
+import random
+import string
+
+if __name__ == '__main__':
+	test_cases = np.linspace(1, 100, num=50)
+	for test_case in test_cases:
+		with open(f"../{int(test_case)}.DAT", 'w') as f:
+			s = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase) for _ in range(int(test_case)))
+			f.write(f"{s}\n")
+			f.write(f"{s.swapcase()}\n")
