e117af3b6aecd4c0ab216567c2400397a437de5a
raresraf <raresfolea97@gmail.com>
2021-10-16 12:54:06 +0300

    898 w/ generator

--- 898B/generator/generator.py
+++ 898B/generator/generator.py
@@ -2,14 +2,19 @@ import numpy as np
 import random
 import string
 
+def max(a, b):
+	if a > b:
+		return a
+	return b
+
 if __name__ == '__main__':
-	test_cases = np.linspace(1, 100000, num=50)
+	test_cases = np.linspace(1, 10000000, num=50)
 	for test_case in test_cases:
 		with open(f"../{int(test_case)}.DAT", 'w') as f:
 			n = int(test_case)
-			f.write(f"{n}\n")
-			for _ in range(n):
-				f.write(f"{random.randint(1,1000000)} {random.randint(1,1000000)}\n")
+			f.write(f"{random.randint(n - random.randint(1, 1999),n)}\n")
+			f.write(f"{random.randint(max(0, n - random.randint(1, n)),n)}\n")
+			f.write(f"{random.randint(max(0, n - random.randint(1, n)),n)}\n")
 
 
 
