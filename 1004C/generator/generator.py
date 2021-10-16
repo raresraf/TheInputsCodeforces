f026125f7274a42ff46f68108541aa796a441da5
raresraf <raresfolea97@gmail.com>
2021-10-16 14:40:59 +0300

    1004C

---
+++ 1004C/generator/generator.py
@@ -0,0 +1,14 @@
+import numpy as np
+import random
+import string
+
+if __name__ == '__main__':
+	test_cases = np.linspace(1, 100000, num=50)
+	for test_case in test_cases:
+		with open(f"../{int(test_case)}.DAT", 'w') as f:
+			n = int(test_case)
+			f.write(f"{n}\n")
+			for i in range(1, n + 1):
+				c = np.random.randint(1, 100000)
+				f.write(f"{c} ")
+			f.write(f"\n")
